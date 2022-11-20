5ad66f2c39e15fba1edd7b4cce1c787fb2e1ee18
Maaitrayo Das <dasmaaitrayo@gmail.com>
2022-11-20 12:58:36 +0530

    ip check 2

--- pages/chat.py
+++ pages/chat.py
@@ -27,7 +27,8 @@ message = st.text_input('Message')
 def chatInterface():
     for names in current_user:
         if names in ["Maaitrayo", "Hrittik", "koushik", "Hrishabh"]:
-            if st.button("Clear Chat", key=1):
+            clear = st.button("Clear Chat", key=1):
+            if clear:
                 chat_data.clear()
 
     publish = st.button("Publish")
